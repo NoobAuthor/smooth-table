bb2939312a801ca4cc6789731af699ea4610e2b9
NoobAuthor <igor21.assis@outlook.com>
2024-09-26 21:48:21 -0300

    Testing zustand

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './app.css'
+import { create } from 'zustand'
+
+interface CounterState {
+  count: number
+  increment: () => void
+}
+
+const useCounterStore = create<CounterState>(set => ({
+  count: 0,
+  increment:() => set(state => ({ count: state.count + 1}))
+}))
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const {count, increment} = useCounterStore()
 
   return (
     <>
@@ -18,9 +29,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {/* <button onClick={() => setCount((count) => count + 1)}> */}
+        {/*   count is {count} */}
+        {/* </button> */}
+        <button onClick={increment}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
